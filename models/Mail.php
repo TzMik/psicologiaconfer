d34990fbd211e81a1cb9224420bb2701914ab413
Mikel <canteromikel@gmail.com>
2022-04-12 20:05:56 -0500

    Fix: set content type as html

--- models/Mail.php
+++ models/Mail.php
@@ -2,14 +2,16 @@
 class Mail {
     private const to = "canteromikel@gmail.com";
     private const subject = "Contacto a través de la web";
+    private const headers = "Content-type: text/html";
     private $message;
 
     public function __construct($message) {
         $this->message = $message;
+
     }
 
     public function send()
     {
-        return mail(self::to, self::subject, $this->message);
+        return mail(self::to, self::subject, $this->message, self::headers);
     }
 }
