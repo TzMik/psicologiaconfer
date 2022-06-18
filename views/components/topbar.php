ed05b7568eddc7b1978011ecd501be8e0b88b00b
Mikel Cantero <canteromikel@gmail.com>
2022-06-18 13:10:05 -0500

    seccion libros en menu

--- views/components/topbar.php
+++ views/components/topbar.php
@@ -21,6 +21,9 @@
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="<?= ROOT_PATH ?>/#opiniones">Opiniones</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="<?= ROOT_PATH ?>/libros">Libros</a>
+                </li>
             </ul>
             <!-- <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
