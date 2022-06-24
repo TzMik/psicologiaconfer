ab012c72ce39e0da84857306a5711fe8ec115928
Mikel Cantero <canteromikel@gmail.com>
2022-06-24 16:26:16 -0500

    Fix: bug arreglado en busqueda

--- actions/searchBooks.php
+++ actions/searchBooks.php
@@ -1,9 +1,16 @@
 <?php
+require_once __DIR__ . "/../config/specialRoutes.php";
 require_once __DIR__ . "/../models/Book.php";
+require_once __DIR__ . "/../helper/UrlGenerator.php";
+require_once __DIR__ . "/../helper/TextUtil.php";
 
 $response = [];
 if (!empty($_POST['listSize'])) {
     $listSize = intval($_POST['listSize']);
     $response = Book::search($_POST['query'], $listSize);
+    foreach ($response as $key => $book) {
+        $response[$key]->description = TextUtil::getFirstWords($book->description);
+        $response[$key]->url = ROOT_PATH . "/libro/" . UrlGenerator::createUrlCanonical($book->title) . "-" . $book->id;
+    }
 }
 echo json_encode($response);
--- js/books.js
+++ js/books.js
@@ -1,23 +1,3 @@
-const getFirstWords = (text, numberOfWords = 10) => {
-    let words = text.split(' ');
-    let result = "";
-    if (words.length <= numberOfWords) {
-        result = text;
-    } else {
-        result = words.slice(0, numberOfWords).join(' ') + '...';
-    }
-    return result;
-}
-
-const getCanonical = (string) => {
-    string = string.trim();
-    string = string.toLowerCase();
-    string = string.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u').replace('ñ', 'n');
-    string = string.replace(/[^(\w\s)]*/, '');
-    string = string.replace(/\s/g, '-');
-    return string;
-}
-
 const getBooksHTML = (data) => {
     let html = "";
     data.forEach(element => {
@@ -27,9 +7,9 @@ const getBooksHTML = (data) => {
                 <img src="${element.image}" onerror="this.src = 'https://kravmaganewcastle.com.au/wp-content/uploads/2017/04/default-image-620x600.jpg';return true;" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${element.title}</h5>
-                    <p class="card-text">${getFirstWords(element.description)}</p>
+                    <p class="card-text">${element.description}</p>
                     <div class="book-btn-div">
-                        <a href="${root_path + '/libro/' + getCanonical(element.title)}-${element.id}" class="btn btn-outline-primary">Ver libro</a>
+                        <a href="${element.url}" class="btn btn-outline-primary">Ver libro</a>
                     </div>
                 </div>
             </div>
