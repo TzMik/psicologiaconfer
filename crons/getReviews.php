66958ab70abe0fdf3a87382bdd50dd8f62f4d006
Mikel Cantero <canteromikel@gmail.com>
2022-06-28 17:28:19 -0500

    Fix: olvidar warnings y mesnaje terminado

--- crons/getReviews.php
+++ crons/getReviews.php
@@ -1,6 +1,8 @@
 <?php
+error_reporting(E_ERROR | E_PARSE);
 
 require_once(__DIR__ . "/../models/CrawlerReviews.php");
 
 $doctoraliaCrawler = new CrawlerReviews("https://www.doctoralia.com.mx/maria-fernanda-quiroga-vidrio/psicologo/guadalajara#tab=profile-reviews");
 $doctoraliaCrawler->getReviews();
+echo "Done!";
